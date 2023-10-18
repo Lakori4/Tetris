7f0e59afc9e7557584fe8a61829bef7d5bd30e9e
Santiago Valderrama <santivalde04@gmail.com>
2023-10-18 11:47:32 +0200

    Change resolution to 600x800

--- TerraRental/src/App.java
+++ TerraRental/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        new Display("Hola hola", 1920, 1080);
+        new Display("Hola hola", 600, 800);
     }
 }
